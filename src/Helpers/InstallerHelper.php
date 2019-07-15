3f5529a7cdc2c764b907369f34a7cb10e98d4ae1
Rob Attfield <emailme@robertattfield.com>
2019-07-15 17:48:27 +1200

    Resolving issue with helper function not working in view files.

---
+++ src/Helpers/InstallerHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace RachidLaasri\LaravelInstaller\Helpers;
+
+
+class InstallerHelper
+{
+    /**
+     * Set the active class to the current opened menu.
+     *
+     * @param  string|array $route
+     * @param  string       $className
+     * @return string
+     */
+    public static function isActive($route, $className = 'active')
+    {
+        if (is_array($route)) {
+            return in_array(Route::currentRouteName(), $route) ? $className : '';
+        }
+        if (Route::currentRouteName() == $route) {
+            return $className;
+        }
+        if (strpos(URL::current(), $route)) return $className;
+    }
+}
